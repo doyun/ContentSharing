984445525a8c8663d6650e8a851cf4ab03308273
Mykyta Doiun <doyun.nikita@gmail.com>
2016-03-10 18:49:23 +0200

    Delete files

--- recepient.js
+++
@@ -1,6 +0,0 @@
-var Recepient = function (id, name, photo) {
-    this.id = id;
-    this.name = name;
-    this.photo = photo;
-    return this;
-}
--- set-vk-token.js
+++
@@ -1,8 +0,0 @@
-
-
-(function () {
-    var matching = location.href.match(/access_token=([^&]*)/);
-    chrome.storage.sync.set({ 'vkaccess_token': matching[1] }, function () {
-        console.log('Token saved');
-    });
-})();
